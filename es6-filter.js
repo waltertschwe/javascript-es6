5d195dc696862b40a455ce810b0c7e525ea0f264
waltertcshwe <wschweitzer@gmail.com>
2017-02-01 00:06:03 -0500

    adding es6-filter an array of objects example

---
+++ es6-filter.js
@@ -0,0 +1,17 @@
+/* How to use the fat arrow to filter an 
+   array of objects in ES6
+
+*/
+
+var stocks = [{"ticker":"MSFT", "price": 68},
+              {"ticker":"GOOG",  "price": 815},
+              {"ticker":"AAPL", "price": 125 },
+              {"ticker":"GE", "price": 30 }];
+
+// get all stocks that have a price of less than 100
+var results = stocks.filter( ticker => ticker.price < 100);
+
+console.log( "stocks with a price less than $100 a share = " , results);
+
+
+
